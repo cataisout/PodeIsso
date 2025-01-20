4967d31939092722a701077e5e4e7287737a2813
Gustavo Gorgônio <ggorgon03@gmail.com>
2025-01-20 10:52:28 -0300

    making routing debug on deploy

--- app/utils/calendar_tools.py
+++ app/utils/calendar_tools.py
@@ -2,15 +2,15 @@ import transformers
 from transformers import pipeline
 import os
 
-#f = open("/app/utils/calendar_context.txt", "r")
-#context = f.read()
+f = open("app/utils/calendar_context.txt", "r")
+context = f.read()
 
 model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
 nlp = pipeline("question-answering", model=model_name)
 
 def calendar_response(question):
-    #result = nlp(question=question, context=context)
-    #answer= result['answer']
-    answer = str(os.listdir('app/utils/'))
+    result = nlp(question=question, context=context)
+    answer= result['answer']
+    #answer = str(os.listdir('app/utils/'))
 
     return answer
