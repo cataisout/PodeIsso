1b8e51d63d287b70f1bdf87e209d6b2641025d06
Gustavo Gorgônio <ggorgon03@gmail.com>
2025-01-20 05:24:13 -0300

    update calendar model pipelines and experiments

--- app/utils/calendar_tools.py
+++ app/utils/calendar_tools.py
@@ -1,9 +1,14 @@
+import transformers
+from transformers import pipeline
 
+f = open("calendar_context.txt", "r")
+context = f.read()
 
-
+model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
+nlp = pipeline("question-answering", model=model_name)
 
 def calendar_response(question):
-
-    answer= 'resposta'
+    result = nlp(question=question, context=context)
+    answer= result['answer']
 
     return answer
