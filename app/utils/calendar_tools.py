7bae98fe866be413de7d304c056c6f45f59797b4
Gustavo Gorgônio <ggorgon03@gmail.com>
2025-01-20 10:51:18 -0300

    making routing debug on deploy

--- app/utils/calendar_tools.py
+++ app/utils/calendar_tools.py
@@ -11,6 +11,6 @@ nlp = pipeline("question-answering", model=model_name)
 def calendar_response(question):
     #result = nlp(question=question, context=context)
     #answer= result['answer']
-    answer = str(os.listdir('app/'))
+    answer = str(os.listdir('app/utils/'))
 
     return answer
