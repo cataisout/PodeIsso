a83140c7cf7261221f4bd4e39161b9fbd7906979
Gustavo Gorgônio <ggorgon03@gmail.com>
2025-01-20 10:28:17 -0300

    fix bug

--- app/utils/calendar_tools.py
+++ app/utils/calendar_tools.py
@@ -2,15 +2,14 @@ import transformers
 from transformers import pipeline
 import os
 
-#f = open("./calendar_context.txt", "r")
-#context = f.read()
+f = open("/app/utils/calendar_context.txt", "r")
+context = f.read()
 
 model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
 nlp = pipeline("question-answering", model=model_name)
 
 def calendar_response(question):
-    #result = nlp(question=question, context=context)
-    #answer= result['answer']
+    result = nlp(question=question, context=context)
+    answer= result['answer']
 
-    answer = str(os.listdir())
     return answer
