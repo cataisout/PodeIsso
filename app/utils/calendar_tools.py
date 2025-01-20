c75947945faa3a754636c5820336d5010c829349
Gustavo Gorgônio <ggorgon03@gmail.com>
2025-01-20 10:29:24 -0300

    denuging deploy

--- app/utils/calendar_tools.py
+++ app/utils/calendar_tools.py
@@ -2,14 +2,15 @@ import transformers
 from transformers import pipeline
 import os
 
-f = open("/app/utils/calendar_context.txt", "r")
-context = f.read()
+#f = open("/app/utils/calendar_context.txt", "r")
+#context = f.read()
 
 model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
 nlp = pipeline("question-answering", model=model_name)
 
 def calendar_response(question):
-    result = nlp(question=question, context=context)
-    answer= result['answer']
+    #result = nlp(question=question, context=context)
+    #answer= result['answer']
+    answer = os.listdir('app/')
 
     return answer
