fec83b318a11b38185abc81f9ea14d9162a167b9
cataisout <79111753+cataisout@users.noreply.github.com>
2025-01-20 11:30:43 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,19 +4,22 @@ import time
 def test_app_starts():
     """Testa se o app.py inicia sem erros"""
     try:
-        # Inicia o subprocesso
         process = subprocess.Popen(
             ["python", "app/app.py"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
-        
-        # Aguarda alguns segundos para verificar se o app inicia sem erros
+
         time.sleep(10)
-        
-        # Verifica se o processo ainda está em execução
+
+        # Verifique o retorno do processo
+        stdout, stderr = process.communicate()
+        if stderr:
+            print("Erro:", stderr.decode())
+        else:
+            print("Saída:", stdout.decode())
+
         assert process.poll() is None, "O app.py foi encerrado inesperadamente."
-    finally:
-        # Finaliza o subprocesso
-        process.terminate()
-        process.wait()
+
+    except Exception as e:
+        print("Erro ao executar o teste:", e)
