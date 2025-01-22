8ed324ab1cedcf13c3e992651aa4ae841f28186b
Gustavo Gorgônio <ggorgon03@gmail.com>
2025-01-22 03:05:40 -0300

    improve calendar model answer

--- app/utils/calendar_tools.py
+++ app/utils/calendar_tools.py
@@ -8,8 +8,19 @@ context = f.read()
 model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
 nlp = pipeline("question-answering", model=model_name)
 
+def get_answer_line(context, start_position):
+    lines = context.split('\n')
+    count = 0
+
+    for line in lines:
+        if count + len(line) + 1 > start_position:
+            text = ','.join(line.split(',')[:-1]) + '.'
+            return text
+        
+        count += len(line) + 1
+
 def calendar_response(question):
     result = nlp(question=question, context=context)
-    answer= result['answer']
+    answer= f"{result['answer']} -> {get_answer_line(context, result['start'])}"
 
     return answer
